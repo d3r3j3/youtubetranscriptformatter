0d235262d330a8082827a029878a38b07ed2eb2c
Dereje <derejelambert@gmail.com>
2021-05-25 22:19:53 -0600

    blank page

---
+++ youtubetranscriptformatter/src/layouts.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class NavBar extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Youtube Transcript Formatter</h1>
+            </div>
+        )
+    }
+}
+
+export default NavBar;
