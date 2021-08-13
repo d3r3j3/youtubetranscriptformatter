22a698c96b62e3980f7b7e79d4f66ab4106389eb
Dereje <derejelambert@gmail.com>
2021-08-12 19:14:40 -0600

    data fix

--- youtubetranscriptformatter/src/pages/Home.js
+++ youtubetranscriptformatter/src/pages/Home.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import {NavBar, IntroBar, OptionBar, Donate, CopyRight} from '../layouts.js';
 
-const data = {
-  title: "Our Services",
-  info: "Welcome to YT Transcript MSTR! A youtube transcript formatter that doesn't require downloading files to use the transcript. Just click COPY and you are all ready to use the transcript!",
-
-}
-
 function Home() {
 
   return (
