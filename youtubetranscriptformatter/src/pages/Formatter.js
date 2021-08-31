580347738ed0a866d68e7c4119590bb8465aa8f9
brain-ruibowu <brain.wuruibo@gmail.com>
2021-08-30 23:03:09 -0700

    update

--- youtubetranscriptformatter/src/pages/Formatter.js
+++ youtubetranscriptformatter/src/pages/Formatter.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import {NavBar, LinkBar} from '../layouts'
-=======
-import {NavBar, LinkBar, Donate, CopyRight} from '../layouts'
->>>>>>> 22a698c96b62e3980f7b7e79d4f66ab4106389eb
 import queryString from 'query-string';
 
 export class Formatter extends Component {
@@ -19,16 +15,8 @@ export class Formatter extends Component {
             <div class="w-full h-full bg-gray grid grid-rows flex justify-center items-center">
                 <NavBar/>
                 <LinkBar urlParam={this.state.vUrl !== undefined ? this.state.vUrl : ''}/>
-<<<<<<< HEAD
                 
                 
-=======
-                <CopyRight />
-                <Donate />
-                <div class="w-full h-full bg-gray fixed -z-10">
-                    &nbsp;
-                </div>
->>>>>>> 22a698c96b62e3980f7b7e79d4f66ab4106389eb
             </div>
         )
     }
--- youtubetranscriptformatter/src/pages/Generator.js
+++ youtubetranscriptformatter/src/pages/Generator.js
@@ -12,13 +12,8 @@ function Generator() {
         <div class="w-full grid grid-rows flex justify-center items-center">
             <NavBar />
             <IntroBar title={data.title} info={data.info} />
-<<<<<<< HEAD
             
             
-=======
-            <CopyRight />
-            <Donate />
->>>>>>> 22a698c96b62e3980f7b7e79d4f66ab4106389eb
         </div>
     );
 }
